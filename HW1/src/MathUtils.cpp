10ae604922647ee95c567660de770a8ef106a331
Saanvi Mammai <reachsaanvi21@gmail.com>
2025-09-04 14:44:20 -0400

    Update MathUtils.cpp

--- HW1/src/MathUtils.cpp
+++ HW1/src/MathUtils.cpp
@@ -3,6 +3,11 @@
 
 
 namespace MathUtils {
-    int add(int a, int b);
-    int multiply(int a, int b);
+    int add(int a, int b){
+        return a+b;
+    }
+    
+    int multiply(int a, int b){
+        return a*b;
+    }
 }
